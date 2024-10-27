3184707e0a0ea0625b06bee2da1cc302a624f9a0
sraveshnandan <kumarsravesh39@gmail.com>
2024-10-27 15:07:07 +0530

    initial

--- dist/database/models/session.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Session = void 0;
-const mongoose_1 = require("mongoose");
-const SessionSchema = new mongoose_1.Schema({}, { timestamps: true });
-const Session = (0, mongoose_1.model)("Session", SessionSchema);
-exports.Session = Session;
--- dist/routes/health.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const health_controllers_1 = require("../controllers/health.controllers");
-const router = (0, express_1.Router)();
-router.route("/health").get(health_controllers_1.CheckHealth);
-exports.default = router;
